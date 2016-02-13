300bd48ddead4799fb698f5ab49142f6b482411d
zhuoensea <simbad.cn@gmail.com>
2016-02-13 12:19:59 +0100

    Update

--- MyApplication/app/src/main/java/com/example/zhuo/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/zhuo/myapplication/MainActivity.java
@@ -6,12 +6,14 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    /******** 任何活动中都应该重写onCreate()方法 ********/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //引用布局文件activity_main.xml
+        /******** LogCat ********/
         //逐级递增：Log.v()日志信息，Log.d()调试信息，Log.i()重要数据，Log.w()警告信息，Log.e()错误信息
+        //传入两个参数：第一个参数tag，用于对打印信息进行过滤；第二个参数msg，即要打印的内容
         Log.d("MainActivity", "onCreate execute");
-        //第一个参数tag，用于对打印信息进行过滤；第二个参数msg，即要打印的内容
     }
 }
