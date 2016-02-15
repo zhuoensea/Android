7e304f7bee05f83e576c8b7bacb1dec67f013c87
zhuoensea <simbad.cn@gmail.com>
2016-02-15 17:00:25 +0100

    启动另一个Activity

---
+++ MyFirstApp/app/src/main/java/com/example/zhuo/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,27 @@
+package com.example.zhuo.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the view as the activity layout
+        setContentView(textView);
+
+    }
+}
--- MyFirstApp/app/src/main/java/com/example/zhuo/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/example/zhuo/myfirstapp/MyActivity.java
@@ -1,13 +1,27 @@
 package com.example.zhuo.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.zhuo.mefirstapp.MESSAGE"; // 设定键名
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
     }
+
+    public void sendMessage(View view) { //与android:onClick属性中提供的方法名字匹配
+        Intent intent = new Intent(this, DisplayMessageActivity.class); //Intent绑定两个不同的组件
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message); //携带键值对
+        startActivity(intent);
+    }
+
 }
