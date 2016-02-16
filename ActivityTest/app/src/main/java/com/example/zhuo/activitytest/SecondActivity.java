c9e6dc3257d5d4c59a4047505d4475ebddb9ca48
zhuoensea <simbad.cn@gmail.com>
2016-02-16 15:51:43 +0100

    singleTop

---
+++ ActivityTest/app/src/main/java/com/example/zhuo/activitytest/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.zhuo.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("SecondActivity", this.toString());
+        setContentView(R.layout.activity_second);
+
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
