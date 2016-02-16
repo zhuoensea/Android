e06e51eef4010e241f6e94e0e3f957171f37111f
zhuoensea <simbad.cn@gmail.com>
2016-02-16 11:11:36 +0100

    Activity life cycle

---
+++ ActivityLifeCycleTest/app/src/main/java/com/example/zhuo/activitylifecycletest/Normal.java
@@ -0,0 +1,13 @@
+package com.example.zhuo.activitylifecycletest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Normal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal);
+    }
+}
--- MyFirstApp/app/src/main/java/com/example/zhuo/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/example/zhuo/myfirstapp/MyActivity.java
@@ -3,6 +3,9 @@ package com.example.zhuo.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
